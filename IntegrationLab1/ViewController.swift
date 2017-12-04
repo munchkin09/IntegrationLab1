2707cfbf241bc9c6bd4ce45c9ba031fbe8e587ca
CarlosL <carlos.developing@gmail.com>
2017-12-04 21:53:02 +0100

    -Initial commit

---
+++ IntegrationLab1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntegrationLab1
+//
+//  Created by Carlos Ledesma on 4/12/17.
+//  Copyright © 2017 Carlos Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
